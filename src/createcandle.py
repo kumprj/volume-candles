0fb5f39dd13eeb5ff22605ace049643243002adc
Robert Kump <kumprj@miamioh.edu>
2020-04-19 18:52:14 -0400

    continuing refactor and clean up

--- src/createcandle.py
+++ src/createcandle.py
@@ -86,7 +86,6 @@ def generateAverage(start_time, end_time, etf):
     current_lookback = 0
 
     while calculate_average == True:
-
         calculate_avg_candles = requests.get(f'https://finnhub.io/api/v1/stock/candle?symbol={etf}&resolution=1&from={start_time}&to={end_time}&token={finnhub_token}')
         print(f'https://finnhub.io/api/v1/stock/candle?symbol={etf}&resolution=1&from={start_time}&to={end_time}&token={finnhub_token}')
         avg_etf_candle = calculate_avg_candles.json()
@@ -119,17 +118,70 @@ def generateAverage(start_time, end_time, etf):
 
     return candle_queue
 
-def createCandles(etf):
-    
-    connection = rds_connect()
-    cursor = connection.cursor()
-    select_query = f'select endtime from public.customcandle_lasttime where type = \'{type_of_candle}\' and etf = \'{etf}\''
-    cursor.execute(select_query)
-    stored_time = cursor.fetchone() # Fetch the end time value from our table if it exists. Point of restart for the script. Only ever one element here.
-    cursor.close()
-    connection.close()
 
-    # If we have no time value stored, start with ETF Inception
+def create_vol_candle(etf, etf_candle, df, num_candles_for_avg, average_volume, candle_queue):
+    current_candle_count = 1
+    current_volume = 0
+    current_candle_high = 0.0 
+    current_candle_low = 0.0
+    first_candle_open = 0.0
+
+    # Break this into functin once we're certain all the details are ironed out.
+    for close, high, low, open_, volume, time in zip(etf_candle['c'], etf_candle['h'], etf_candle['l'], etf_candle['o'], etf_candle['v'], etf_candle['t']):
+        average = int(average_volume / num_candles_for_avg)
+        current_volume += int(volume)
+        high = float(high)
+        low = float(low)
+        close = float(close)
+        open_ = float(open_)
+        # print(f'added {volume} to current volume: {current_volume}. Current Average is {average} and will make new candle once current volume passes it.')
+        # print('--- new loop item ---')
+        # We want our Volume Candle, which may represent many time candles, to have its open and close be the *open of the first candle* 
+        # and the *close of the last candle*. 
+        if (first_candle_open == 0.0):
+            first_candle_open = open_
+
+        # Calculate our lowest and highest value for the volume candle.
+        current_candle_high = close if close > current_candle_high and close >= open_ else current_candle_high
+        current_candle_high = open_ if open_ > current_candle_high and open_ > close else current_candle_high
+        if current_candle_low == 0.0:
+            current_candle_low = open_ if open_ < close else close
+        else:
+            current_candle_low = close if close < current_candle_low and close < open_ else current_candle_low
+            current_candle_low = open_ if open_ < current_candle_low and open_ < close else current_candle_low
+
+        # Average is the sum of the Candle Queue, representing two weeks of data, divided by its length. We want to create a new candle
+        # every time volume hits that average. This is contrary to time-based candles where you make a new one every minute.
+        # One volume candle may be three-1 minute bars one time, then seven the next. This if-block handles that creation and stores in a dataframe.
+        if current_volume > average:
+            current_candle_time = dt.datetime.utcfromtimestamp(time).strftime("%m/%d/%Y %H:%M")
+            insert_args = (current_candle_time, first_candle_open, close, current_candle_high, current_candle_low,
+                            etf, type_of_candle, current_volume)
+            store_row(df, insert_args)
+            # print(f'New volume candle created using {current_candle_count} volume periods.') # Turn on for logging purpose.
+            # Reset all of our current values for the next candle.
+            current_volume = 0
+            current_candle_high = 0.0
+            current_candle_low = 0.0
+            first_candle_open = 0.0
+            current_candle_count = 1
+        # If-block fails? Maintain our current count and move to next time candle.
+        else:
+            # print('Did not create candle. Adding next candle to total volume.')
+            current_candle_count += 1
+        # End if block for inserting
+        
+        remove_volume = candle_queue.popleft()
+        average_volume -= remove_volume
+        candle_queue.append(volume)
+        average_volume += volume
+        # print(f'appended {volume} to list and popped off: {remove_volume} for ETF: {etf}')
+        # print('--- end candle ---')
+        # end For loop
+    return df
+
+def instantiate_time_period(etf, stored_time):
+    # If we have no time value stored, start with ETF Inception.
     if (stored_time == None):
         # Calculate relevant ETF inception time. Varies slightly per ticker so need to generate some edge cases.
         time_to_inception = fifteen_years_unix if (etf == 'XOP' or etf == 'XME') else twenty_years_unix
@@ -143,16 +195,35 @@ def createCandles(etf):
     else:
         stored_time = int(stored_time[0])
         end_time = int(tm.time())
-        start_time = stored_time
+        start_time = stored_time   
 
-    # Variables for the while Loop.
+    return end_time, start_time, stored_time
+
+
+def fetch_stored_time(etf):
+    connection = rds_connect()
+    cursor = connection.cursor()
+    select_query = f'select endtime from public.customcandle_lasttime where type = \'{type_of_candle}\' and etf = \'{etf}\''
+    cursor.execute(select_query)
+    stored_time = cursor.fetchone() # Fetch the end time value from our table if it exists. Point of restart for the script. Only ever one element here.
+    cursor.close()
+    connection.close()
+    return stored_time
+
+
+def createCandles(etf):
+
+    # Instantiate our variables.
+    stored_time = fetch_stored_time(etf)
+    end_time, start_time, stored_time = instantiate_time_period(etf, stored_time)    
     last_run = False
-    candle_queue = collections.deque([])
     isFirstRun = True
+    candle_queue = collections.deque([])
     num_candles_for_avg = 0
     average_volume = 0 
     df_columns = ["enddate", "open", "close", "high", "low", "ticker", "type", "candle_volume"]
     df = pd.DataFrame(columns=df_columns)
+
     # If its our first run, we want to decrement from the start time period and calculate an average. 
     # We are using 1 minute bars, so add ~2 weeks of candles to a queue. Queue stores our candle average size.
     if isFirstRun:
@@ -162,102 +233,28 @@ def createCandles(etf):
         for vol in candle_queue:
             average_volume += int(vol)
 
-    # Large Loops could probably be optimized into functions. Once code is tested, refactor for this.
-
-
     while stored_time >= start_time:
         get_candle = requests.get(f'https://finnhub.io/api/v1/stock/candle?symbol={etf}&resolution=1&from={start_time}&to={end_time}&token={finnhub_token}')
         etf_candle = get_candle.json()
         
         # If we happen to find a 'no_data' but we are still loading data, just continue. Its a one-off.
         if (etf_candle['s'] == 'no_data'):
-            print(f'etf {etf} had no results at time period {start_time} to {end_time}') # Logging assistance
-            tm.sleep(2) # Pause to not overload
+            # print(f'etf {etf} had no results at time period {start_time} to {end_time}') # Logging assistance
             # If we find a no_data but its also the last run, break the loop.
-            if last_run == True:
+            if last_run == True:  
                 break
             else:
                 last_run, start_time, end_time = update_time_interval(last_run, start_time, end_time, increment_time, stored_time)
+                tm.sleep(2) # Pause to not overload
                 continue
 
-        current_candle_count = 1
-        current_volume = 0
-        current_candle_high = 0.0 
-        current_candle_low = 0.0
-        first_candle_open = 0.0
-
-        # Break this into functin once we're certain all the details are ironed out.
-        for close, high, low, open_, volume, time in zip(etf_candle['c'], etf_candle['h'], etf_candle['l'], etf_candle['o'], etf_candle['v'], etf_candle['t']):
-            # print('--- new loop item ---')
-            average = int(average_volume / num_candles_for_avg)
-            current_volume += int(volume)
-            high = float(high)
-            low = float(low)
-            close = float(close)
-            open_ = float(open_)
-            # We want our Volume Candle, which may represent many time candles, to have its open and close be the *open of the first candle* 
-            # and the *close of the last candle*. 
-            if (first_candle_open == 0.0):
-                first_candle_open = open_
-
-            # print(f'added {volume} to current volume: {current_volume}. Current Average is {average} and will make new candle once current volume passes it.')
-
-            # Calculate our highest value for the volume candle.
-            current_candle_high = close if close > current_candle_high and close >= open_ else current_candle_high
-            current_candle_high = open_ if open_ > current_candle_high and open_ > close else current_candle_high
-            # Calculate our lowest for the volume candle.
-            if current_candle_low == 0.0:
-                current_candle_low = open_ if open_ < close else close
-            else:
-                current_candle_low = close if close < current_candle_low and close < open_ else current_candle_low
-                current_candle_low = open_ if open_ < current_candle_low and open_ < close else current_candle_low
-
-            # Average is the sum of the Candle Queue, representing two weeks of data, divided by its length. We want to create a new candle
-            # every time volume hits that average. This is contrary to time-based candles where you make a new one every minute.
-            # One volume candle may be three-1 minute bars one time, then seven the next. This if-block handles that.
-            # The SQL Insert needs to be broken out into a new method.
-            if current_volume > average:
-                current_candle_time = dt.datetime.utcfromtimestamp(time).strftime("%m/%d/%Y %H:%M")
-                insert_args = (current_candle_time, first_candle_open, close, current_candle_high, current_candle_low,
-                                etf, type_of_candle, current_volume)
-                store_row(df, insert_args)
-                
-                # print(f'New volume candle created using {current_candle_count} volume periods.') # Turn on for logging purpsoes
-
-                # Reset all of our current values for the next candle.
-                current_volume = 0
-                current_candle_high = 0.0
-                current_candle_low = 0.0
-                first_candle_open = 0.0
-                current_candle_count = 1
-                
-
-            # If-block fails? Maintain our current count and move to next time candle.
-            else:
-                # print('Did not create candle. Adding next candle to total volume.')
-                current_candle_count += 1
-            # End if block for inserting
-            
-            # End of For Loop:
-            # We don't want to give us O(n^3) complexity with these loops by recalculating the total volume from the entire queue of 3000-4000 elements.
-            # However, we want to maintain a queue of the last ~2 weeks to ensure our current candle is being created based on a
-            # 'relevant' volume average. Volume in 2005 is a lot different than volume in 2020.
-            # Basing it off recent periods (EMA kind of idea) gives the candles more merit.
-            # Continue to maintain the queue, but subtract the removed element and add the new element to the sum. No need
-            # to recalculate every time. We popleft because that is the oldest element, and append on the right to label it the newest.
-            remove_volume = candle_queue.popleft()
-            average_volume -= remove_volume
-
-            candle_queue.append(volume)
-            average_volume += volume
-            # print(f'appended {volume} to list and popped off: {remove_volume} for ETF: {etf}')
-            # print('--- end candle ---')
-            # end For loop
+        # Loop through our JSON object to create the candles.
+        df = create_vol_candle(etf, etf_candle, df, num_candles_for_avg, average_volume, candle_queue)
 
         # Last stage of while Loop. After we loop through an entire API query, we store the df, increment time and do it again.
         engine = create_engine(f'postgresql://{database_user}:{database_password}@{database_host}:{database_port}/{database_db}')
         con = engine.connect()
-        df.to_sql('customcandle', engine, schema='public', index=False, if_exists="append")
+        df.to_sql('customcandle2', engine, schema='public', index=False, if_exists="append")
         con.close()
         engine.dispose()
         df = df.iloc[0:0]
@@ -266,16 +263,11 @@ def createCandles(etf):
             break
         else:
             last_run, start_time, end_time = update_time_interval(last_run, start_time, end_time, increment_time, stored_time)
-        
     # end while loop
 # end function
 
-
-
 def generateCandles():
 
-    # Multithread our candle generation. Each ETF is independent so speeds up data entry because ~15-20 years takes awhile.
-    # Create new thread for each process calling createCandles method. After they complete, join them all.
     for etf in etf_list:
         createCandles(etf)
 
@@ -285,70 +277,14 @@ def generateCandles():
         sql_delete = f'delete from public.customcandle_lasttime where type = \'{type_of_candle}\' and etf = \'{etf}\''
         cursor.execute(sql_delete)
         connection.commit()
+
         # Insert new value for endtime used on future/present day runs.
         end_time = tm.time()
         sql_insert = f'insert into public.customcandle_lasttime (endtime, type, etf) values ({end_time}, \'{type_of_candle}\', \'{etf}\')'
         cursor.execute(sql_insert)
         connection.commit()
-
         cursor.close()
         connection.close()
-    # threads = []
-    # for i in range(5):
-    #     thread = threading.Thread(target=createCandles, args=(etf_list[i],))
-    #     thread.start()
-    #     threads.append(thread)
-    #     print(f'started thread {i}')
-    #     tm.sleep(10)
-    # for thread in threads:
-    #     thread.join()
-    # threads = []
-    # for i in range(5,9):
-    #     thread = threading.Thread(target=createCandles, args=(etf_list[i],))
-    #     thread.start()
-    #     threads.append(thread)
-    #     print(f'started thread {i}')
-    #     tm.sleep(10)
-    # for thread in threads:
-    #     thread.join()
-    # threads = []
-    # for i in range(8,13):
-    #     thread = threading.Thread(target=createCandles, args=(etf_list[i],))
-    #     thread.start()
-    #     threads.append(thread)
-    #     print(f'started thread {i}')
-    #     tm.sleep(10)
-    # for thread in threads:
-    #     thread.join()
-
-    # for i in range(len(etf_list)):
-    #     thread = threading.Thread(target=createCandles, args=(etf_list[i],))
-    #     thread.start()
-    #     threads.append(thread)
-    #     print(f'started thread {i}')
-    #     tm.sleep(10)
-    
-    # for thread in threads:
-    #     thread.join()
-
-
-    # connection = rds_connect()
-    # cursor = connection.cursor()
-
-    # # Delete currently stored value of endtime. We only want one stored at a time.
-    # sql_delete = f'delete from public.customcandle_lasttime where type = \'{type_of_candle}\''
-    # cursor.execute(sql_delete)
-    # connection.commit()
-
-    # # Insert new value for endtime used on future/present day runs.
-    # end_time = tm.time()
-    # sql_insert = f'insert into public.customcandle_lasttime (endtime, type) values ({end_time}, \'{type_of_candle}\')'
-    # cursor.execute(sql_insert)
-    # connection.commit()
-
-    # cursor.close()
-    # connection.close()
-
 
 def main():
     generateCandles()  
